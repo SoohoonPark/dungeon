b5520d00e29dda85db0d6edb302dbd51a992f5c1
Hoon <Hoon@192.168.200.142>
2019-01-17 19:45:06 +0900

    인벤토리 UI 기능 구현 테스트

--- myJava/src/miniProject/InventoryScreen.java
+++ myJava/src/miniProject/InventoryScreen.java
@@ -1,6 +1,9 @@
 package miniProject;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.LinkedList;
@@ -20,30 +23,53 @@ public class InventoryScreen extends JFrame {
 	public InventoryScreen(List<String> inventory) {
 		System.out.println("[info] 인벤토리 열림");
 		this.inventory = inventory;
-		//System.out.println("[info] 인벤토리 크기 : " + inventory.size());
+		System.out.println("[info] 인벤토리 크기 : " + inventory.size());
 		createInventoryScreen();
 	}
 
 	public void createInventoryScreen() {
 		setTitle("인벤토리");
-		setBounds(1210, 495, 300, 210);
+		setBounds(1210, 495, 350, 210);
 		setLayout(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		DefaultListModel<String> inventorydata = new DefaultListModel<String>();
+		while(inventory.iterator().hasNext()) {
+			inventorydata.addElement(inventory.iterator().next());
+		}
 		JList<String> inventorylist = new JList<String>(inventorydata);
 		JScrollPane scroll = new JScrollPane(inventorylist);
 		scroll.setBounds(10, 10, 200, 160);
 		
 		
 		JPanel panelButtons = new JPanel(null);
-		panelButtons.setBounds(220, 10, 60, 160);
+		panelButtons.setBounds(220, 10, 100, 160);
 		panelButtons.setBorder(new LineBorder(Color.RED));
 		
 		JButton btnUseitem = new JButton("사용");
+		btnUseitem.setBounds(10, 10, 80, 30);
+		btnUseitem.setFont(new Font("굴림", Font.PLAIN, 14));
+		
 		JButton btnDropitem = new JButton("버리기");
+		btnDropitem.setBounds(10, 50, 80, 30);
+		btnDropitem.setFont(new Font("굴림", Font.PLAIN, 14));
+		
 		JButton btnClose = new JButton("닫기");
+		btnClose.setBounds(10, 90, 80, 30);
+		btnClose.setFont(new Font("굴림", Font.PLAIN, 14));
+		btnClose.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				dispose();
+			}
+		});
+		
+		panelButtons.add(btnUseitem);
+		panelButtons.add(btnDropitem);
+		panelButtons.add(btnClose);
 		
 		add(scroll);
 		add(panelButtons);
@@ -52,13 +78,8 @@ public class InventoryScreen extends JFrame {
 		// 윈도우리스너 (해당 창이 닫히면 리스너에서 GameScreen의 능력치 버튼을 활성화(setEnabled(true)) 해줌.
 		addWindowListener(new WindowAdapter() {
 			public void windowClosed(WindowEvent e) {
-				System.out.println("앗");
 				GameScreen.btnInventory.setEnabled(true);
 			}
 		});
-	}
-	
-	public static void main(String[] args) {
-		new InventoryScreen(null);
 	}
 }
